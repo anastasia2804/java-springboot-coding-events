863dcb7e14043c194fc36da6d488e81b36ef725e
Anastasia <anastasia2804@gmail.com>
2023-02-15 13:17:58 -0500

    created first model

--- src/main/java/com/example/codingevents/controllers/EventController.java
+++ src/main/java/com/example/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package com.example.codingevents.controllers;
 
+import com.example.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,7 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
@@ -31,7 +32,7 @@ public class EventController {
     }
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+        events.add(new Event(eventName));
         return "redirect:/events";
     }
 
